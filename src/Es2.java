624c9a9ffd08c8e050414440587dbf90d496c004
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-07-22 16:24:33 +0200

    commit esercizio 2

--- src/Es2.java
+++ src/Es2.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Es2 {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Inserisci la prima Stringa: ");
+        String string1 = in.nextLine();
+        System.out.println("Inserisci la seconda Stringa: ");
+        String string2 = in.nextLine();
+        System.out.println("Inserisci la terza Stringa: ");
+        String string3 = in.nextLine();
+
+
+        String concat = string1 + " " + string2 + " " + string3;
+
+
+        String reversedConcat = new StringBuilder(concat).reverse().toString();
+
+        System.out.println("La concatenazione delle stringhe è: " + reversedConcat);
+
+        in.close();
+    }
 }
